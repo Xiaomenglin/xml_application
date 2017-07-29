6aa3d653a4ce25e7538135527234871d0d5e48e8
Xiaomenglin <xml3171@163.com>
2017-07-29 10:58:22 +0800

    two git

---
+++ JDay0729.java
@@ -0,0 +1,11 @@
+public class JDay0729{
+	public static void main(String[] args){
+		//String s="";
+		StringBuilder ss=new StringBuilder();
+		for(int i=0;i<100;i++){
+			ss.append(i);
+		}
+		String s=ss.toString();
+		System.out.println(s);
+	}
+}
