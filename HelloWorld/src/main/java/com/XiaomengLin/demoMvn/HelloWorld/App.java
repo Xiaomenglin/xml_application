58c74bfb6a884b172c64a7509ab41d95e7bcda24
Xiaomenglin <xmlin3171@163.com>
2017-09-02 17:14:14 +0800

    DAY0902

---
+++ HelloWorld/src/main/java/com/XiaomengLin/demoMvn/HelloWorld/App.java
@@ -0,0 +1,13 @@
+package com.XiaomengLin.demoMvn.HelloWorld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
