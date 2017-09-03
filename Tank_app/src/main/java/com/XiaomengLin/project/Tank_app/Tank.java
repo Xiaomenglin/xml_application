b4a9acc39112316c5cfceeedf4a76e031862955c
Xiaomenglin <xmlin3171@163.com>
2017-09-03 10:21:04 +0800

    day090302

---
+++ Tank_app/src/main/java/com/XiaomengLin/project/Tank_app/Tank.java
@@ -0,0 +1,15 @@
+package com.XiaomengLin.project.Tank_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class Tank 
+	
+{
+	
+//    public static void main( String[] args )
+//    {
+//        System.out.println( "Hello World!" );
+//    }
+}
