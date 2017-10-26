f20d854d16baff10bb929672856c625bd175b9dd
Xiaomenglin <xmlin3171@163.com>
2017-10-26 11:20:18 +0800

    2017102601

--- Tank_app/src/main/java/com/XiaomengLin/project/Tank_app/Tank1.java
+++ Tank_app/src/main/java/com/XiaomengLin/project/Tank_app/Tank1.java
@@ -36,12 +36,56 @@ class MyPanel extends JPanel{
 	public void paint(Graphics g){
 		super.paint(g);
 		g.fillRect(0, 0, 400, 300);
-		g.setColor(Color.YELLOW);
-		g.fill3DRect(myTank.getX(), myTank.getY(), 5, 30, false);
-		g.fill3DRect(myTank.getX()+15, myTank.getY(), 5, 30, false);
-		g.fill3DRect(myTank.getX()+5, myTank.getY()+5, 10, 20, false);
-		g.fillOval(myTank.getX()+5, myTank.getY()+10, 10, 10);
-		g.drawLine(myTank.getX()+10, myTank.getY()+15, myTank.getX()+10, myTank.getY()-3);
+		this.drawTank(myTank.getX(), myTank.getY(), g, 3, 0);
+//		g.setColor(Color.YELLOW);
+//		g.fill3DRect(myTank.getX(), myTank.getY(), 5, 30, false);
+//		g.fill3DRect(myTank.getX()+15, myTank.getY(), 5, 30, false);
+//		g.fill3DRect(myTank.getX()+5, myTank.getY()+5, 10, 20, false);
+//		g.fillOval(myTank.getX()+5, myTank.getY()+10, 10, 10);
+//		g.drawLine(myTank.getX()+10, myTank.getY()+15, myTank.getX()+10, myTank.getY()-3);
+	}
+	
+	public void drawTank(int x,int y,Graphics g,int fangxiang,int leixing){
+		switch(leixing){
+		case 0://我的坦克
+			g.setColor(Color.YELLOW);
+			break;
+		case 1://敌人的坦克
+			g.setColor(Color.GREEN);
+			break;
+		
+		}
+		switch(fangxiang){//坦克方向
+		case 0://方向：上
+			g.fill3DRect(x, y, 5, 30, false);
+			g.fill3DRect(x+15, y, 5, 30, false);
+			g.fill3DRect(x+5, y+5, 10, 20, false);
+			g.fillOval(x+5, y+10, 10, 10);
+			g.drawLine(x+10, y+15, x+10, y-3);
+			break;
+		case 1://方向：左
+			g.fill3DRect(x, y, 30, 5, false);
+			g.fill3DRect(x, y+15, 30, 5, false);
+			g.fill3DRect(x+5, y+5, 20, 10, false);
+			g.fillOval(x+10, y+5, 10, 10);
+			g.drawLine(x+15, y+10, x-3, y+10);
+			break;
+		case 2://方向：下
+			g.fill3DRect(x, y, 5, 30, false);
+			g.fill3DRect(x+15, y, 5, 30, false);
+			g.fill3DRect(x+5, y+5, 10, 20, false);
+			g.fillOval(x+5, y+10, 10, 10);
+			g.drawLine(x+10, y+15, x+10, y+33);
+			break;
+		case 3://方向：右
+			g.fill3DRect(x, y, 30, 5, false);
+			g.fill3DRect(x, y+15, 30, 5, false);
+			g.fill3DRect(x+5, y+5, 20, 10, false);
+			g.fillOval(x+10, y+5, 10, 10);
+			g.drawLine(x+15, y+10, x+33, y+10);
+			break;
+			
+		}
 	}
 }
 class Tank{
